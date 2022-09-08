e3a1c22964f6ed1b7ddb5347b03668132ca938ff
edafe ajirigit config --global user.name edafe <edafeajiri5@gmail.comgit config --global user.email edafeajiri5@gmail.com>
2022-09-08 07:31:24 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle"); return (0);
-
-
-
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
