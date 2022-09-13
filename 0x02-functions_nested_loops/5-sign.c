6e08d172f2f2fedfb15d7d71724aa6d12f357a1f
edafe ajirigit config --global user.name edafe <edafeajiri5@gmail.comgit config --global user.email edafeajiri5@gmail.com>
2022-09-13 11:12:52 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  * @n: the int to check
  *
  * Return: 1 and prints + if n is greater than zero
- * 
+ *
  * 0 and prints 0 if n is zero
  *
  * -1 and prints - if n is less than zero
@@ -15,7 +15,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		return (0);
 	}
 	else if (n == 0)
 	{
