f84de4e283f498caffc5f431e463181e39074c5c
edafe ajirigit config --global user.name edafe <edafeajiri5@gmail.comgit config --global user.email edafeajiri5@gmail.com>
2022-09-13 18:39:23 -0700

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * prints_times_table - prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
+ * Description : times table for n
  */
 void print_times_table(int n)
 {
