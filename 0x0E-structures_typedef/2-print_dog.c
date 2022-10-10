e54353e1ebaf921f8bfc75a8d79a272fd2b9861a
edafe ajirigit config --global user.name edafe <edafeajiri5@gmail.comgit config --global user.email edafeajiri5@gmail.com>
2022-10-10 08:29:12 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 		if (!(d->name))
 			printf("Name: (nil)\n");
 		else
-			printf("Name: (nil)\n");
+			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
 
